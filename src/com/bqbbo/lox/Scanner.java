6f97290622b97426de6ffb7d7c188c9be7bb9b72
Bobby Elmore <bobbye23899@gmail.com>
2025-03-15 23:03:55 -0700

    String scanning

--- src/com/bqbbo/lox/Scanner.java
+++ src/com/bqbbo/lox/Scanner.java
@@ -118,10 +118,33 @@ class Scanner {
                 line++;
                 break;
 
+            case '"':
+                string();
+                break;
+
             default:
                 Lox.error(line, "Unexpected character \"" + c + "\"");
                 break;
         }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string");
+        }
+
+        advance(); // Consume the ending " character
+
+        String stringLiteral = sourceCode.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, stringLiteral);
     }
 
     // Return 'true' if the character after 'c' is the expected character
