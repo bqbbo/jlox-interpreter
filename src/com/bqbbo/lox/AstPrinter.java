5aae363d831c003e139ceb444395c3a10df0c2de
Bobby Elmore <bobbye23899@gmail.com>
2025-03-18 09:06:47 -0700

    AstPrinter Class for debugging, remove `public` from Lox.java

---
+++ src/com/bqbbo/lox/AstPrinter.java
@@ -0,0 +1,56 @@
+package com.bqbbo.lox;
+
+class AstPrinter implements Expr.Visitor<String> {
+    /*
+     * public static void main(String[] args) {
+     * Expr expression = new Expr.Binary(
+     * new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new
+     * Expr.Literal(123)),
+     * new Token(TokenType.STAR, "*", null, 1), new Expr.Grouping(new
+     * Expr.Literal(42.2)));
+     *
+     * System.out.println(new AstPrinter().print(expression));
+     * }
+     */
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("GROUPING", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) {
+            return "nil";
+        }
+
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("(").append(name);
+        for (Expr expr : exprs) {
+            stringBuilder.append(" ");
+            stringBuilder.append(expr.accept(this));
+        }
+        stringBuilder.append(")");
+
+        return stringBuilder.toString();
+    }
+}
