ef1458c6a5d0c135d0bf892e9b95ae7f0ba68cf0
Bobby Elmore <bobbye23899@gmail.com>
2025-03-18 12:09:37 -0700

    Parser Class

---
+++ src/com/bqbbo/lox/Parser.java
@@ -0,0 +1,12 @@
+package com.bqbbo.lox;
+
+import java.util.List;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+}
