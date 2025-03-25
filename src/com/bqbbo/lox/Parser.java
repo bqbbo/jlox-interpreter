346464bb9954679f9ff589b233dc14a370b4a4e7
Bobby Elmore <bobbye23899@gmail.com>
2025-03-25 15:50:10 -0700

    Local/Global Variable Scoping; Block Syntax and Execution

--- src/com/bqbbo/lox/Parser.java
+++ src/com/bqbbo/lox/Parser.java
@@ -47,6 +47,10 @@ class Parser {
             return printStatement();
         }
 
+        if (match(TokenType.LEFT_BRACE)) {
+            return new Stmt.Block(blockStatement());
+        }
+
         return expressionStatement();
     }
 
@@ -73,6 +77,17 @@ class Parser {
         Expr expr = expression();
         consume(TokenType.SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> blockStatement() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(TokenType.RIGHT_BRACE, "Expect '}' after block");
+        return statements;
     }
 
     private Expr assignment() {
